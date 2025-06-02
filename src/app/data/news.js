7591b00281b50dfd3823bdf427f246a09205d2bb
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-02 20:19:47 +0200

    Feature/Data-news

---
+++ src/app/data/news.js
@@ -0,0 +1,28 @@
+// data/data-news.js
+
+export const newsItems = [
+  {
+    id: 1,
+    title: "Enter Yahweh’s Creations",
+    date: "June 1, 2025",
+    image: "/images/news-yahweh.jpg",
+    description:
+      "The cathedral doors have opened. Dive into an eerie world of divine horror in the latest chapter of the game.",
+  },
+  {
+    id: 2,
+    title: "Supporter Pack Released",
+    date: "May 28, 2025",
+    image: "/images/news-supporter-pack.jpg",
+    description:
+      "Show your support and gain access to exclusive items with the Yahweh's Supporter Pack. Available now on Steam.",
+  },
+  {
+    id: 3,
+    title: "Our Creations Teaser",
+    date: "May 21, 2025",
+    image: "/images/news-creations.jpg",
+    description:
+      "Unmask the horrors within. A sneak peek into the terrifying creations haunting Yahweh’s domain.",
+  },
+];
