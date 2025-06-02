e0e3db68d8e2284ffd5ad1c1f495bedc9cf8e4e2
Endorze <alexyurihallgren@hotmail.com>
2025-06-02 15:30:06 +0200

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>How many pickles are in a picklejar?</h1>
+      <h1>This is the testing ground.</h1>
     </div>
   );
 }
