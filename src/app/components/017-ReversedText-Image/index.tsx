6f26a082e452f9873712262e2d7402cce038c052
andrew <andrewkalumba29@gmail.com>
2025-06-12 01:24:11 +0200

    update

--- src/app/components/017-ReversedText-Image/index.tsx
+++ src/app/components/017-ReversedText-Image/index.tsx
@@ -1,7 +1,9 @@
 import ReusableTextImage from "../006-Resusable-Text-Image"
+import TextImage from "../014-Feature/Text-Image"
 const Reversed = () => {
     return (
-        <ReusableTextImage title={""} text={""} image={""}/>
+        // <ReusableTextImage title={""} text={""} image={""}/>
+        <TextImage/>
     )
 }
 export default Reversed
