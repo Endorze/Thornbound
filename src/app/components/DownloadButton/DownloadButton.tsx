da57536f9b20442360fe5bdac4733d95e52e6bc8
Endorze <alexyurihallgren@hotmail.com>
2025-06-11 13:15:09 +0200

    feat: Added download button

--- src/app/components/DownloadButton/DownloadButton.tsx
+++ src/app/components/DownloadButton/DownloadButton.tsx
@@ -1,8 +1,10 @@
 const DownloadButton = () => {
     return (
-        <a href="/">
-            <img src={"/download.png"} className="w-[194px] h-[92px]"/>
-        </a>
+        <div>
+            <a href="https://futuregames.itch.io/thornbound" target="_blank">
+                <img src={"/download.png"} className="w-[194px] h-[92px]"/>
+            </a>
+        </div>
     )
 }
 
