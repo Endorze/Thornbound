92a5aa3a4b30315175bf1d21828ec11a555c5712
Endorze <alexyurihallgren@hotmail.com>
2025-06-11 16:03:37 +0200

    feat: Added a conditional hero section

--- src/app/components/ReusableHero/ReusableHero.tsx
+++ src/app/components/ReusableHero/ReusableHero.tsx
@@ -1,3 +1,5 @@
+import DownloadButton from "../DownloadButton/DownloadButton";
+
 type Props = {
     title?: string,
     mainHero: boolean,
@@ -5,15 +7,15 @@ type Props = {
 
 const ReusableHero = ({title, mainHero}: Props) => {
     return (
-        <div className="hidden lg:flex bg-[url('/herolotus.png')] bg-cover bg-center w-full min-h-screen  text-white justify-center relative pt-12 pb-24">
+        <div className="flex bg-[url('/herolotus.png')] bg-cover bg-center w-full h-[700px]  text-white justify-center relative pt-12 pb-24 px-2">
             {mainHero && (
                 <div className="max-w-[900px] flex flex-col justify-center items-center">
-                    <img src={"/thornbound.png"} className="max-w-[701px] h-auto"/>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, quaerat provident animi fuga, corrupti iure possimus asperiores ab vitae distinctio sed eum? Commodi repellat, alias officia tenetur laborum accusamus eaque.</p>
+                    <img src={"/thornbound.png"} className="lg:max-w-[701px] h-auto"/>
+                    <p className="mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, quaerat provident animi fuga, corrupti iure possimus asperiores ab vitae distinctio sed eum? Commodi repellat, alias officia tenetur laborum accusamus eaque.</p>
+                    <DownloadButton />
                 </div>
             )}
-
-            <h2 className="">{title}</h2>
+            <h2 style={{fontSize: "104px"}} className="flex items-center">{title}</h2>
         </div>
     )
 }
