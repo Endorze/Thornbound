54e2c72db9e4644a8db3ad37c6f8f60009e481e8
vineetha <vineethaveerapu4@gmail.com>
2025-06-11 18:05:24 +0200

    adding h1 reusable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import ImageCarousel from "./components/Carousel/Carousel";
 import images from "@/data/images";
+import H1Title from "./components/H1Title";
 
 export default function Home() {
   return (
+    <>
+    <H1Title text="Thornbound" size="100px" />
     <div className="w-full p-2 flex flex-col items-center justify-center">
-      <ImageCarousel images={images} title="Kravia Manor" text1="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla" text2="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla"/>
-    </div>
+        <ImageCarousel images={images} title="Kravia Manor" text1="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla" text2="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla"/>
+      </div>
+    </>
   );
 }
