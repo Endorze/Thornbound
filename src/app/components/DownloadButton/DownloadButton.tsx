396842a42dbd2ead126add19e1bbb2577af7a5f6
Endorze <alexyurihallgren@hotmail.com>
2025-06-11 13:15:08 +0200

    feat: Supersimple downloadbutton that navigates to futuregames.itch.io

---
+++ src/app/components/DownloadButton/DownloadButton.tsx
@@ -0,0 +1,9 @@
+const DownloadButton = () => {
+    return (
+        <a href="/">
+            <img src={"/download.png"} className="w-[194px] h-[92px]"/>
+        </a>
+    )
+}
+
+export default DownloadButton;
