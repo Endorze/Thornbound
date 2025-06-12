dae91848b37b67d060ea3d5287ac1fe914c6b5ff
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-12 11:15:59 +0200

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import ReusableH3Title from "./components/ReusableH3Title/ReusableH3Title";
 
 
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -14,6 +15,4 @@ export default function Home() {
     </div>
   );
 }
-
-
 
