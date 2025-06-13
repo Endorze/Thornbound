f59fb454ee2d46903c2ba527e68853bd7a4873f3
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-14 00:12:11 +0200

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,18 @@
-
 import ImageCarousel from "./components/Carousel/Carousel";
 import images from "@/data/images";
 import ReusableHero from "./components/ReusableHero/ReusableHero";
 import H2Title from "./components/H2Title/H2Title";
 import H1Title from "./components/H1Title";
 import ReusableTextImage from "./components/006-Resusable-Text-Image";
-import TransparentContainer from "@/app/components/TransparentContainerReusable";
-
-
+import TransparentContainerReusable from "@/app/components/TransparentContainerReusable";
 
 export default function Home() {
   return (
-    <>
-      <H1Title text="Thornbound" size="100px" />
+    <div className="min-h-screen bg-gradient-to-b from-black to-gray-900 text-white flex flex-col items-center justify-center p-8 sm:p-20">
+      <TransparentContainerReusable>
+        <H1Title text="Thornbound" size="100px" />
 
-      <div className="w-full p-2 flex flex-col items-center justify-center">
-        <TransparentContainerReusable>
+        <div className="w-full p-2 flex flex-col items-center justify-center">
           <ReusableHero mainHero={true} />
           <ImageCarousel
             images={images}
@@ -35,8 +32,8 @@ export default function Home() {
             image="/image.png"
             reversed={true}
           />
-        </TransparentContainerReusable>
-      </div>
-    </>
+        </div>
+      </TransparentContainerReusable>
+    </div>
   );
 }
