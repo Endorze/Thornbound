eeb7e0cfddd601de23d04ca89d290a8ddb61866c
vineetha <vineethaveerapu4@gmail.com>
2025-06-13 11:54:01 +0200

    adding features component

---
+++ src/app/components/Features/index.tsx
@@ -0,0 +1,47 @@
+import ReusableTextImage from "../006-Resusable-Text-Image";
+import H1Title from "../H1Title";
+
+const Features = () => {
+  return (
+    <>
+      <div className="text-center">
+        <H1Title text="Features" size="60px" />
+      </div>
+      <div className="p-4 flex flex-col gap-4 w-full">
+        <ReusableTextImage
+
+          title="Headline"
+          text="Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction.
+
+            Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction
+
+            "
+          image="/thornbound.png"
+        />
+
+        <ReusableTextImage
+          title="Headline"
+          text="Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction.
+
+            Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction
+
+            "
+          image="/thornbound.png"
+        />
+
+        <ReusableTextImage
+          title="Headline"
+          text="Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction.
+
+            Embark on a perilous journey through the cursed Thornbound realm, where ancient magic and dark forces intertwine. Master powerful abilities, forge alliances with mysterious beings, and uncover the secrets of a world teetering on the edge of destruction
+
+            "
+          image="/thornbound.png"
+          reversed={true}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Features;
