737758ebcf0fe9084bee36616b775a51be420658
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-13 23:46:04 +0200

    removed lore section will do in seprate ticket

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import TransparentContainerResuseable from "@/app/components/TransparentContainerResuseable/TransparentContainerResuseable";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Sex laxar i en laxask</h1>
+    <div className="min-h-screen bg-gradient-to-b from-black to-gray-900 text-white flex items-center justify-center p-8 sm:p-20">
+      <TransparentContainerResuseable>
+        <h1 className="text-4xl font-semibold tracking-wide text-center">Sex laxar i en laxask</h1>
+      </TransparentContainerResuseable>
     </div>
   );
 }
