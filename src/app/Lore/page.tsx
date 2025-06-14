29c9b860820b07ae0bbb28b7c5808b095426b736
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-14 16:49:51 +0200

    i will merge this code into dev

---
+++ src/app/Lore/page.tsx
@@ -0,0 +1,10 @@
+import LoreSection from "@/app/components/LoreSection/page";
+
+
+export default function Lore() {
+  return (
+    <section className="relative w-full min-h-screen text-white">
+      <LoreSection />
+    </section>
+  );
+}
