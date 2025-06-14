bb41afebad1ba04bd2ac4e6c2f3a3203891a7407
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-14 20:39:58 +0200

    fixed transparent containter coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import TransparentContainerResuseable from "@/app/components/TransparentContainerResuseable";
+
+
+
+
+import TransparentContainerReusable from "@/app/components/TransparentContainerReusable";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-black to-gray-900 text-white flex items-center justify-center p-8 sm:p-20">
-      <TransparentContainerResuseable>
-        <h1 className="text-4xl font-semibold tracking-wide text-center">Sex laxar i en laxask</h1>
-      </TransparentContainerResuseable>
+    <div className="min-h-screen bg-blue-600 text-white flex items-center justify-center p-8">
+      <TransparentContainerReusable>
+        <h1 className="text-4xl font-bold text-center">Sex laxar i en laxask</h1>
+      </TransparentContainerReusable>
     </div>
   );
 }
