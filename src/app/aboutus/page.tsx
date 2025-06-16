eac50a87a97e9f05530008889a63ead215b0c92b
Moon2024-light <brite_moon2008@yahoo.com>
2025-06-17 01:55:19 +0200

    fixed coding for import components

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,8 +1,14 @@
+
+import ReusableHero from "@/app/components/ReusableHero/ReusableHero";
 import FeatureReusableCredits from "@/app/components/FeatureReusableCredits/FeatureReusableCredits";
 
 export default function AboutUs() {
   return (
     <main>
+     
+      <ReusableHero mainHero={false} title="ABOUT US" />
+
+     
       <FeatureReusableCredits />
     </main>
   );
