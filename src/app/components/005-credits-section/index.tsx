1f8fca03c9d207b92501d05ae3a065850919b42f
Moshira89 <mushiraaljak@example.com>
2025-06-17 16:05:19 +0200

    updated

--- src/app/components/005-credits-section/index.tsx
+++ src/app/components/005-credits-section/index.tsx
@@ -1,36 +1,35 @@
+'use client';
+
 import Image from "next/image";
 
 const CreditsSection = () => {
   return (
-    <div className="relative w-full h-auto bg-[url('/herolotus.png')] bg-cover bg-center text-white px-4 py-16">
-      <h1 className="text-[48px] md:text-[100px] font-[400] leading-tight font-cinzel text-center mx-auto shadow-[0_0_16px_rgba(0,0,0,0.8)] border border-black w-full max-w-[449px]">
-        CREDITS
-      </h1>
-
-      <div className="mt-12 bg-black/40 backdrop-blur-md shadow-[4px_4px_12px_rgba(0,0,0,0.8)] flex flex-col lg:flex-row items-center justify-between p-6 gap-8 max-w-[1264px] mx-auto">
-        <div className="w-full lg:w-1/2 text-[16px] md:text-[20px] leading-[28px] font-vollkorn text-justify">
+    <section className="w-full bg-black/40 backdrop-blur-md text-white px-4 py-16">
+      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center gap-10">
+        
+        <div className="w-full lg:w-1/2 text-base md:text-lg leading-relaxed font-vollkorn text-justify space-y-6">
           <p>
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+            In addition to the assets created by our artists, assets from online marketplaces such as Fab and CGtrader were used to further assist our artists, in regards to both characters and props.
           </p>
-          <p className="mt-4">
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          <p>
+
+            These assets sourced from the marketplace have been used in a way where the artists alter the meshes or textures in order to fit the art direction of the game and to keep the involvement of the artists throughout the whole production process.
           </p>
-          <p className="mt-4">
-            Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam...
-          </p>
+          
         </div>
 
-        <div className="w-full lg:w-[532px] h-auto">
+        <div className="w-full lg:w-[532px] flex justify-center">
           <Image
-            src="/images/angel.png"
-            alt="Credits Illustration"
+            src="/images/barry.png"
+            alt="Credits illustration"
             width={532}
             height={375}
-            className="object-contain w-full h-auto"
+            className="object-contain w-full h-auto rounded-xl shadow-lg"
+            priority
           />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
