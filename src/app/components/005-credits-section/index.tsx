e3093ff0775bdc77ff26b12cbe049dc4ac12d710
Moshira89 <mushiraaljak@example.com>
2025-06-18 15:41:47 +0200

    updating data

--- src/app/components/005-credits-section/index.tsx
+++ src/app/components/005-credits-section/index.tsx
@@ -1,32 +1,32 @@
 'use client';
-
-import Image from "next/image";
+import ReusableTextImage from "../006-Resusable-Text-Image";
 
 const CreditsSection = () => {
+  const descriptionParagraphs = [
+    "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
+    "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?",
+    "Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+  ];
+
+  const combinedText = descriptionParagraphs.join("\n\n");  
+
   return (
     <section className="w-full bg-black/40 backdrop-blur-md text-white px-4 py-16">
-      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center gap-10">
-        
-        <div className="w-full lg:w-1/2 text-base md:text-lg leading-relaxed font-vollkorn text-justify space-y-6">
-          <p>
-            In addition to the assets created by our artists, assets from online marketplaces such as Fab and CGtrader were used to further assist our artists, in regards to both characters and props.
-          </p>
-          <p>
-            These assets sourced from the marketplace have been used in a way where the artists alter the meshes or textures in order to fit the art direction of the game and to keep the involvement of the artists throughout the whole production process.
-          </p>
-          
-        </div>
+      <div className="max-w-6xl mx-auto flex flex-col gap-12">
 
-        <div className="w-full lg:w-[532px] flex justify-center">
-          <Image
-            src="/images/angel.png"
-            alt="Credits illustration"
-            width={532}
-            height={375}
-            className="object-contain w-full h-auto rounded-xl shadow-lg"
-            priority
-          />
-        </div>
+        <ReusableTextImage
+          title=""  
+          image="/images/credit-picture1.png"
+          text={combinedText}
+        />
+
+        <ReusableTextImage
+          title=""
+          image="/images/credit-picture1.png"
+          text={combinedText}
+        />
+        
       </div>
     </section>
   );
