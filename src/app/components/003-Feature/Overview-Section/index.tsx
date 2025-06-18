5b6cef930cde40a9a6edf93dad5036759f14f70d
Saba Waheed <saba_sh27@hotmail.com>
2025-06-18 17:51:12 +0200

    added overview

---
+++ src/app/components/003-Feature/Overview-Section/index.tsx
@@ -0,0 +1,22 @@
+import ReusableTextImage from "../../006-Resusable-Text-Image";
+
+const OverviewSection = () => {
+    return (
+        <>
+            <div className="text-center mb-10">
+                <h2 className="text-5xl md:text-7xl text-white">OVERVIEW</h2>
+            </div>
+
+            <ReusableTextImage
+                title=""
+                text="As Zerim, an experienced retired mage who has a young granddaughter Elira with a deadly illness with no working cure. 
+                        Find and fight your way through the deathtrap that is Castle Kravia, filled with enemies, traps and puzzles. Unveil the forsaken secrets of previous venturers and the castle itself to get a hold of the rare and protected flower Virellis, said to cure any injury and illness or grant Immortality to whoever consumes it, to save Elira. 
+                        Do you think you can make it out alive? "
+                image="https://via.placeholder.com/600x400"
+                reversed={false}
+            />
+        </>
+    );
+};
+
+export default OverviewSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import ImageCarousel from "./components/Carousel/Carousel";
 import images from "@/data/images";
 import ReusableHero from "./components/ReusableHero/ReusableHero";
+import OverviewSection from "./components/003-Feature/Overview-Section";
+
+
+
 
 
 export default function Home() {
@@ -8,7 +12,9 @@ export default function Home() {
     <>
 
     <div className="w-full p-2 flex flex-col items-center justify-center">
+      
       <ReusableHero mainHero={true}/>
+      <OverviewSection />
       <ImageCarousel images={images} title="Kravia Manor" text1="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla" text2="lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla lorem ipsum blablabla"/>
     </div>
    </>
