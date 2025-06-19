7ecb136d016a0583887fcab866d7744d573630a4
Moshira89 <mushiraaljak@example.com>
2025-06-19 23:20:41 +0200

    Add path

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,9 +1,11 @@
+import CreditsSection from "../components/005-credits-section";
 import ReusableHero from "../components/ReusableHero/ReusableHero";
 
 export default function CreditsPage() {
     return (
         <div className="flex flex-col text-center items-center justify-center">
             <ReusableHero title="Credits" mainHero={false}/>
+            <CreditsSection />
         </div>
     )
 }
