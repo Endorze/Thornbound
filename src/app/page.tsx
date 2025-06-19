6c087cf048c8a880a585e55a2227c9c15b87dcb5
Endorze <alexyurihallgren@hotmail.com>
2025-06-19 10:48:27 +0200

    feat: Added Vineethas feature component in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Features from "./components/Features";
+import MaxWidthContainer from "./components/MaxWidthContainer/MaxWidthContainer";
 import ReusableHero from "./components/ReusableHero/ReusableHero";
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-center">
-          <ReusableHero mainHero={true}/>
+        <ReusableHero mainHero={true} />
+        <MaxWidthContainer>
+          <Features />
+        </MaxWidthContainer>
       </div>
     </>
 
