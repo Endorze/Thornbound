abee36dae225262f16f5d5b399e013755a92fa7e
Endorze <alexyurihallgren@hotmail.com>
2025-06-19 10:46:59 +0200

    feat: Added features back to dev (Sorry vineetha)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Features from "./components/Features";
+import MaxWidthContainer from "./components/MaxWidthContainer/MaxWidthContainer";
 import ReusableHero from "./components/ReusableHero/ReusableHero";
 
 export default function Home() {
@@ -5,6 +7,9 @@ export default function Home() {
     <>
       <div className="flex flex-col justify-center">
           <ReusableHero mainHero={true}/>
+          <MaxWidthContainer>
+          <Features />
+          </MaxWidthContainer>
       </div>
     </>
 
