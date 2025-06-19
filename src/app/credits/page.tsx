286e1a0a654f01f7533d22b6edffff8ad5805f37
Moshira89 <mushiraaljak@example.com>
2025-06-19 12:51:09 +0200

    updated

--- src/app/credits/page.tsx
+++ src/app/credits/page.tsx
@@ -1,7 +1,11 @@
+import { CreditsItems } from "@/data/dataFile";
+import CreditsSection from "../components/005-credits-section";
+
 export default function CreditsPage() {
     return (
         <div className="flex flex-col text-center items-center justify-center min-h-screen">
             <h1 className="text-4xl font-bold"> Welcome to the Credits</h1>
+            <CreditsSection />
         </div>
     )
 }
