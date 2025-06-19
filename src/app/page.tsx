82fae120f4a6d9c5083ade15c61a170e37b30a78
andrew <andrewkalumba29@gmail.com>
2025-06-19 11:01:22 +0200

    solved conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Features from "./components/Features";
 import MaxWidthContainer from "./components/MaxWidthContainer/MaxWidthContainer";
 import ReusableHero from "./components/ReusableHero/ReusableHero";
-import CreditsSection from "./components/005-credits-section";
-import H2Title from "./components/H2Title/H2Title";
-import ReusableTextImage from "./components/Resusable-Text-Image";
-import ReversedTextImage from "./components/ReversedImageText";
-import ReusableH3Title from "./components/ReusableH3Title/ReusableH3Title";
 import Features from "./components/Features";
-import FeatureReusableCredits from "./components/FeatureReusableCredits/FeatureReusableCredits";
+
 
 export default function Home() {
   return (
